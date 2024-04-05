4735bdce2bd7590720e8de569392b3a1dd29f604
Medioworks <sandorkolozs@medioworks.com>
2024-04-05 22:26:50 +0200

    bb

--- src/Cikk.php
+++ src/Cikk.php
@@ -10,7 +10,7 @@ class Cikk
 
     public function index($request)
     {
-        
+
         $cikk_azonosito = $request['cikkszam'];
         $partner_id = $request['partner_id'];
 
@@ -26,17 +26,12 @@ class Cikk
         if (isset($kereskedelem_beallitas->cikkparositas)) {
 
             if ($kereskedelem_beallitas->cikkparositas == 'cikk_azonosito') {
-                $cikkszam = ModelsCikk::select("*")
-                    ->where("cikk_azonosito", "=", $cikk_azonosito)
-                    ->get();
 
-                if ($cikkszam->count() == 0) {
-                    $cikkszam = ModelsCikk::select("cikktorzs.*")
-                        ->join('cikk_tcso', 'cikktorzs.id', '=', 'cikk_tcso.cikktorzs_id')
-                        ->where("cikk_tcso.partner_id", "=", $partner_id)
-                        ->where("cikktorzs.cikk_azonosito", "=", $cikk_azonosito)
-                        ->get();
-                }
+                $cikkszam = ModelsCikk::select("cikktorzs.*")
+                    ->join('cikk_tcso', 'cikktorzs.id', '=', 'cikk_tcso.cikktorzs_id')
+                    ->where("cikk_tcso.partner_id", "=", $partner_id)
+                    ->where("cikktorzs.cikk_azonosito", "=", $cikk_azonosito)
+                    ->get();
 
                 if ($cikkszam->count() == 0) {
                     $cikkszam = self::get_noitem($partner_id, $noitem);
@@ -46,17 +41,12 @@ class Cikk
             }
 
             if ($kereskedelem_beallitas->cikkparositas == 'forgalmazoi_kod') {
-                $cikkszam = ModelsCikk::select("*")
+
+                $cikkszam = ModelsCikk::select("cikktorzs.*")
+                    ->join('cikk_tcso', 'cikktorzs.id', '=', 'cikk_tcso.cikktorzs_id')
+                    ->where("cikk_tcso.partner_id", "=", $partner_id)
                     ->where("forgalmazoi_kod", "=", $cikk_azonosito)
                     ->get();
-
-                if ($cikkszam->count() == 0) {
-                    $cikkszam = ModelsCikk::select("cikktorzs.*")
-                        ->join('cikk_tcso', 'cikktorzs.id', '=', 'cikk_tcso.cikktorzs_id')
-                        ->where("cikk_tcso.partner_id", "=", $partner_id)
-                        ->where("forgalmazoi_kod", "=", $cikk_azonosito)
-                        ->get();
-                }
 
                 if ($cikkszam->count() == 0) {
                     $cikkszam = self::get_noitem($partner_id, $noitem);
@@ -66,15 +56,11 @@ class Cikk
             }
         }
 
-        $cikkszam = ModelsCikk::select("cikktorzs.*")
-            ->where("cikk_azonosito", "=", $cikk_azonosito)
-            ->get();
 
-        if ($cikkszam->count() == 0) {
-            $cikkszam = ModelsCikk::select("*")
-                ->where("forgalmazoi_kod", "=", $cikk_azonosito)
-                ->get();
-        }
+
+        $cikkszam = ModelsCikk::select("*")
+            ->where("forgalmazoi_kod", "=", $cikk_azonosito)
+            ->get();
 
         if ($cikkszam->count() == 0) {
             $cikkszam = ModelsCikk::select("cikktorzs.*")
@@ -99,19 +85,9 @@ class Cikk
         return $cikkszam;
     }
 
-    // private static function get_noitem($noitem)
-    // {
-
-    //     return ModelsCikk::select("*")
-    //         ->where("id", "=", $noitem)
-    //         ->get();
-
-    // }
-
 
     private static function get_noitem($partner_id, $tcso)
     {
-
         $cikkszam = ModelsCikk::select("cikktorzs.*")
             ->join('cikk_tcso', 'cikktorzs.id', '=', 'cikk_tcso.cikktorzs_id')
             ->join('kereskedelem_tcso', 'cikk_tcso.kereskedelem_tcso_id', '=', 'kereskedelem_tcso.id')
